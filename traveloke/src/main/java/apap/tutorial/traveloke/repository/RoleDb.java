6a38a9c3e80c3bceb59d7df94cb2ba456881e986
adisryn10 <imadeadisuryanugraha@gmail.com>
2020-11-18 18:21:10 +0700

    finish tutorial 6 web security

---
+++ traveloke/src/main/java/apap/tutorial/traveloke/repository/RoleDb.java
@@ -0,0 +1,13 @@
+package apap.tutorial.traveloke.repository;
+
+import apap.tutorial.traveloke.model.RoleModel;
+import apap.tutorial.traveloke.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RoleDb  extends JpaRepository<RoleModel, Long> {
+    List<RoleModel> findAll();
+}
